4ba03c21c6ec6a7d7e436f8ffe23d2963f5e861d
John Flockton <thegreatercurve@users.noreply.github.com>
2022-05-04 23:05:25 +0100

    add migration script

--- migration.sh
+++ migration.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/env bash
-
-DIRECTORIES = [
-    'lexical'
-    'lexical-react'
-    'lexical-yjs'
-    'lexical-list'
-    'lexical-table'
-    'lexical-file'
-    'lexical-clipboard'
-    'lexical-hashtag'
-    'lexical-history'
-    'lexical-selection'
-    'lexical-offset'
-    'lexical-code'
-    'lexical-plain-text'
-    'lexical-rich-text'
-    'lexical-utils'
-    'lexical-dragon'
-    'lexical-overflow'
-    'lexical-link'
-    'lexical-text'
-    'lexical-markdown'
-]
-
-
-for directory in DIRECTORIES;
+DIRECTORIES=(
+    "lexical" 
+    "lexical-react" 
+    "lexical-yjs" 
+    "lexical-list" 
+    "lexical-table" 
+    "lexical-file" 
+    "lexical-clipboard" 
+    "lexical-hashtag" 
+    "lexical-history" 
+    "lexical-selection" 
+    "lexical-offset" 
+    "lexical-code" 
+    "lexical-plain-text" 
+    "lexical-rich-text" 
+    "lexical-utils" 
+    "lexical-dragon" 
+    "lexical-overflow" 
+    "lexical-link" 
+    "lexical-text" 
+    "lexical-markdown"
+)
+for directory in ${DIRECTORIES[@]}; do
     for i in $(find ./packages/$directory -iname "*.js" -not -path "*__tests__*");
         do git mv "$i" "$(echo $i | rev | cut -d '.' -f 2- | rev).ts";
     done
-    for i in $(find ./packages$directory -iname "*.jsx" -not -path "*__tests__*");
+    for i in $(find ./packages/$directory -iname "*.jsx" -not -path "*__tests__*");
         do git mv "$i" "$(echo $i | rev | cut -d '.' -f 2- | rev).tsx";
     done
 done
